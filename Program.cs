3e5c5d283d701f04053f6d6ea4161f1011500535
Dmitriy Zabrodskiy <dzabrik@yandex.ru>
2022-12-01 18:43:42 +0300

    Program is done

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > max)
+{
+    Console.WriteLine("Большее число - " + number);
+}
+else 
+{
+    Console.WriteLine("Большее число - " + max);
+}
+
